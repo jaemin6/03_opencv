577253751b8222fd9c37d09b5826b87dea42a71f
jaemin6 <gumt555@gmail.com>
2025-07-30 11:43:33 +0900

    수정 완료 근사 컨투어 코드 작성

--- src/cntr_approximate.py
+++ src/cntr_approximate.py
@@ -4,25 +4,25 @@ import numpy as np
 img = cv2.imread('../img/bad_rect.png')
 img2 = img.copy()
 
-# 그레이스케일과 바이너리 스케일 변환
+# 그레이스케일과 바이너리 이미지 변환
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, th = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
 
-# 컨투어찾기
-temp, contours, hierachy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# 컨투어 찾기 (OpenCV 4.x 기준: 2개 리턴)
+contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# 첫 번째 컨투어 가져오기
 contour = contours[0]
 
-# 전체 둘레의 0.05로 오차 범위 지정
+# 둘레 길이의 5% 오차로 근사화
 epsilon = 0.05 * cv2.arcLength(contour, True)
-
-# 근사 컨투어 계산
 approx = cv2.approxPolyDP(contour, epsilon, True)
 
-# 각각 컨투어 선 그리기
+# 원본 컨투어와 근사화된 컨투어 각각 그리기
 cv2.drawContours(img, [contour], -1, (0, 255, 0), 3)
-cv2.drawContours(img2, [approx], -1, (0, 255, 0), 3)
+cv2.drawContours(img2, [approx], -1, (0, 0, 255), 3)
 
-cv2.imshow('contour', img)
-cv2.imshow('approx', img2)
-cv2.waitKey()
+cv2.imshow('Original Contour', img)
+cv2.imshow('Approximated Contour', img2)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
