f6e085d00d6fc3dc3990809ca0ba4ce06f781c88
jaemin6 <gumt555@gmail.com>
2025-07-30 13:19:20 +0900

    히스토그램 시각화

--- src/line_tracer.py
+++ src/line_tracer.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 # 저장된 사진 경로에 맞게 수정
 img = cv2.imread('../img/line_tape.jpg', cv2.IMREAD_GRAYSCALE)
@@ -7,3 +8,6 @@ if img is None:
     print("실패")
 else:
     print("성공")
+
+
+hist = cv2.calcHist([img], [0], None, [256], [0, 256])
