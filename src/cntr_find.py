91dc654b6037011d67f0e2af42e9b411a0adfcd8
jaemin6 <gumt555@gmail.com>
2025-07-30 10:38:32 +0900

    가운데만 컨투어

--- src/cntr_find.py
+++ src/cntr_find.py
@@ -11,33 +11,42 @@ imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # 스레숄드 적용 (흑백 반전)
 ret, imthres = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY_INV)
 
-# 컨투어: 모든 좌표 유지 (CHAIN_APPROX_NONE)
-contour, hierarchy = cv2.findContours(imthres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
 # 컨투어: 꼭지점만 유지 (CHAIN_APPROX_SIMPLE)
 contour2, hierarchy2 = cv2.findContours(imthres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+# 면적 기준 필터링
+target_contours = []
+for c in contour2:
+    area = cv2.contourArea(c)
+    if 2000 < area < 3000:  # 가운데 네모의 크기에 맞춰 값 조정
+        target_contours.append(c)
+
 # 결과 출력
-print('도형의 갯수: %d (%d)' % (len(contour), len(contour2)))
+#print('도형의 갯수: %d (%d)' % (len(contour), len(contour2)))
+print(f"선택된 도형 수: {len(target_contours)}")
 
 # 전체 좌표로 그리기 (초록색)
-cv2.drawContours(img, contour, -1, (0, 255, 0), 4)
+#cv2.drawContours(img, contour, -1, (0, 255, 0), 4)
 
 # 꼭지점 좌표만으로 그리기 (초록색)
-cv2.drawContours(img2, contour2, -1, (0, 255, 0), 4)
+#cv2.drawContours(img2, contour2, -1, (0, 255, 0), 4)
 
 # 전체 좌표를 파란 점으로
-for i in contour:
-    for j in i:
-        cv2.circle(img, tuple(j[0]), 1, (255, 0, 0), -1)
+# for i in contour:
+#     for j in i:
+#         cv2.circle(img, tuple(j[0]), 1, (255, 0, 0), -1)
 
-# 꼭지점 좌표만 파란 점으로
-for i in contour2:
-    for j in i:
-        cv2.circle(img2, tuple(j[0]), 1, (255, 0, 0), -1)
+# # 꼭지점 좌표만 파란 점으로
+# for i in contour2:
+#     for j in i:
+#         cv2.circle(img2, tuple(j[0]), 1, (255, 0, 0), -1)
 
 # 이미지 출력
-cv2.imshow('CHAIN_APPROX_NONE', img)
-cv2.imshow('CHAIN_APPROX_SIMPLE', img2)
+# cv2.imshow('CHAIN_APPROX_NONE', img)
+# cv2.imshow('CHAIN_APPROX_SIMPLE', img2)
+# 가운데 네모만 그림
+cv2.drawContours(img, target_contours, -1, (0, 255, 0), 3)
+
+cv2.imshow('Filtered Center Contour', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
