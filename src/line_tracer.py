5694683883c6e1e960967ea171b2db5fe4acf8bd
jaemin6 <gumt555@gmail.com>
2025-07-30 13:20:42 +0900

    테이프 특성 파악 완료

--- src/line_tracer.py
+++ src/line_tracer.py
@@ -1,13 +1,17 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# 저장된 사진 경로에 맞게 수정
+# 저장된 사진 불러오기
 img = cv2.imread('../img/line_tape.jpg', cv2.IMREAD_GRAYSCALE)
 
 if img is None:
     print("실패")
-else:
-    print("성공")
-
+    exit()
 
 hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+
+plt.plot(hist, color='black')
+plt.title('Grayscale Histogram')
+plt.xlabel('Pixel value')
+plt.ylabel('Pixel count')
+plt.show()
